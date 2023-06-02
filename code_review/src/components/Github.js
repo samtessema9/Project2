ec039b8b35550389d21328eb9794d599210daa01
samtessema9 <99148321+samtessema9@users.noreply.github.com>
2023-06-02 09:51:39 -0700

    added github info logic

--- code_review/src/components/Github.js
+++ code_review/src/components/Github.js
@@ -1,48 +1,75 @@
-import React from 'react';
+import React, {useState} from 'react';
 import axios from 'axios';
 
 const Github = () => {
+
+    const [link, setLink] = useState('')
+
+    const fetchFilesRecursively = async (username, repository, folderPath) => {
+        const apiUrl = `https://api.github.com/repos/${username}/${repository}/contents/${folderPath}`;
+      
+        try {
+          const response = await axios.get(apiUrl);
+          const data = response.data;
+      
+          // Loop through the items in the folder
+          for (const item of data) {
+            if (item.type === 'file') {
+              const fileContentsResponse = await axios.get(item.download_url);
+              const fileContents = atob(fileContentsResponse.content);
+      
+              console.log(`File: ${item.path}`);
+              console.log('Contents:', fileContents);
+            } else if (item.type === 'dir') {
+                await fetchFilesRecursively(username, repository, item.path);
+            }
+          }
+        } catch (error) {
+          console.error('Error reading folder:', error);
+          throw error;
+        }
+      };
+
+      const extractInfoFromUrl = (url) => {
+        const baseUrl = 'https://github.com/'
+        const parts = url.replace(baseUrl, '').split('/')
+        
+        const username = parts[0]
+        const repo = parts[1]
+        const path = parts[parts.length-1]
+        
+        if (username && repo && path) {
+            return {username, repo, path}
+        } else {
+            return false
+        }
+    }
+
+      const handleClick = () => {
+        const info = extractInfoFromUrl(link)
+        if (!info) {
+            console.log('invalid link')
+            return 
+        }
+        console.log(info)
+        
+      }
+
+      const handleChange = (e) => {
+        setLink(e.target.value)
+      }
+
     return ( 
-        <p>Github here</p>
+        <>
+            <input value={link} onChange={handleChange} />
+            <button onClick={handleClick}>submit</button>
+        </>
      );
 }
  
 export default Github;
 
 
-const fetchFilesRecursively = async (username, repository, folderPath) => {
-    const apiUrl = `https://api.github.com/repos/${username}/${repository}/contents/${folderPath}`;
+
   
-    try {
-      const response = await axios.get(apiUrl);
-      const data = response.data;
-  
-      // Loop through the items in the folder
-      for (const item of data) {
-        if (item.type === 'file') {
-          const fileUrl = item.download_url;
-          const fileContentsResponse = await axios.get(fileUrl);
-          const fileContents = atob(fileContentsResponse.content);
-  
-          console.log(`File: ${item.path}`);
-          console.log('Contents:', fileContents);
-        } else if (item.type === 'dir') {
-          await fetchFilesRecursively(username, repository, item.path);
-        }
-      }
-    } catch (error) {
-      console.error('Error reading folder:', error);
-      throw error;
-    }
-  };
-  
-  // Usage example
-  const username = 'your-github-username';
-  const repository = 'your-github-repo';
-  const folderPath = 'path/to/folder';
-  const accessToken = 'your-personal-access-token';
-  
-  fetchFilesRecursively(username, repository, folderPath, accessToken)
-    .catch(error => {
-      // Handle errors
-    });
+ 
